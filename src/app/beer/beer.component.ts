c8452af0edbfebbe110973064abb1278ecbb21bf
V.S.M <sampath.vithanage1@gmail.com>
2021-10-02 10:00:24 +0300

    Initial project

---
+++ src/app/beer/beer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beer',
+  templateUrl: './beer.component.html',
+})
+export class BeerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
